58975cdbe9a63b2b9a19a1b5b5f651ff628db650
pa <pa@arista.com>
2024-12-24 14:57:43 +0000

    make script customizable

--- terminals.sh
+++ terminals.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
+# This script requires VSCode `fabiospampinato.vscode-terminals` extension
+# The script will wait for docker exec to be ready before making the terminal available to user input
+
 clear
 echo 'Waiting for the lab to start...'
-until docker exec -it $1 bash -c "Cli -p 15 -c 'show version' 2> /dev/null" >/dev/null 2>&1; do 
+until docker exec -it $1 ${TERMINAL_SHELL} -c "${TERMINAL_STATUS_COMMAND} 2> /dev/null" >/dev/null 2>&1; do 
     sleep 5
 done
 clear
 echo "$1 is ready."
-# print additional instructions for the user if env var is set
+# Print additional instructions for the user if env var is set
 if [ "${TERMINAL_READY_MESSAGE}" ]; then
     echo ${TERMINAL_READY_MESSAGE}
 fi
-docker exec -it $1 Cli -p 15
+docker exec -it $1 ${TERMINAL_READY_COMMAND}
